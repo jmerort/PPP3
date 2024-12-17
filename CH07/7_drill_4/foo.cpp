b28431d839d154db191c41a3765686e07ee2e461
jmerort <jmerortz@gmail.com>
2024-12-17 08:51:48 +0100

    Add files via upload

---
+++ CH07/7_drill_4/foo.cpp
@@ -0,0 +1,14 @@
+#include "foo.h"
+#include <iostream>
+
+int foo = 0;
+
+void print_foo()
+{
+	std::cout << foo;
+}
+
+void print(int n)
+{
+	std::cout << n;
+}
---
+++ CH07/7_drill_4/use.cpp
@@ -0,0 +1,18 @@
+/*
+use.cpp file used to test the functions from unit 7 drill part 4.
+
+Dec 2024
+@jmerort
+*/
+
+#include <iostream>
+#include "foo.h"
+
+int main()
+{
+	foo = 4;
+	print_foo();
+	print(10);
+
+	return 0;
+}
