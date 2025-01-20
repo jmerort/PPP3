e6577a1a548b47e8c2fa639cd7109fe0df8e5f05
jmerort <jmerortz@gmail.com>
2025-01-20 17:44:11 +0100

    Update 8_13.cpp

--- CH08/8_13.cpp
+++ CH08/8_13.cpp
@@ -9,6 +9,11 @@ multiplication, division, and equality operators. Also, provide a
 conversion to double. Why would people want to use a Rational class?
 ---
 
+TO BE DONE:
+- provide assignment
+- to_double
+- explain why one would use this
+
 Jan 2024
 @jmerort
 */
@@ -24,23 +29,127 @@ Jan 2024
 class Rational 
 {
     public:
-        Rational (int nn, int dd):n{nn},d{dd}{};
+        Rational (int nn, int dd);
 
         int num(){return n;};
         int den(){return d;};
 
-        friend bool std::operator== (Rational, Rational);
+		friend Rational operator+ (Rational&, Rational&);
+		friend Rational operator- (Rational&, Rational&);
+		friend Rational operator* (Rational&, Rational&);
+		friend Rational operator/ (Rational&, Rational&);
+
+        friend bool operator== (Rational&, Rational&);
+		friend bool operator!= (Rational&, Rational&);
+
+        friend std::ostream& operator << (std::ostream&, Rational);
+
+		struct Invalid {};
 
     private:
         int n,
             d;
+
+		void simplify();
 };
 
 
 
-bool std::operator== (Rational a, Rational b)
+Rational::Rational (int nn, int dd):n{nn},d{dd}
+{
+	if (dd == 0) throw Invalid {};
+	//simplify(); //if you want to simplify every rational
+}
+
+
+
+void Rational::simplify()
+{
+	for (int i {2}; i <= n || i <= d; i++)
+	{
+		if (n % i == 0 && d % i == 0)
+		{
+			d = d / i;
+			n = n / i;
+			i = 1; //start counting again
+		}
+	}
+}
+
+
+
+Rational operator+ (Rational& a, Rational& b)
+{
+	int den {a.den()*b.den()},
+	    num {a.num()*b.den() + b.num()*a.den()};
+
+	Rational sum {num, den};
+	sum.simplify();
+
+	return sum;
+}
+
+
+
+Rational operator- (Rational& a, Rational& b)
+{
+	int den {a.den()*b.den()},
+	    num {a.num()*b.den() - b.num()*a.den()};
+
+	Rational sub {num, den};
+	sub.simplify();
+
+	return sub;
+}
+
+
+
+Rational operator* (Rational& a, Rational& b)
+{
+	int den {a.den()*b.den()},
+	    num {a.num()*b.num()};
+
+	Rational mul {num, den};
+	mul.simplify();
+
+	return mul;
+}
+
+
+
+Rational operator/ (Rational& a, Rational& b)
+{
+	if (b.num() == 0) throw Rational::Invalid {}; //prevent division by zero
+
+	int den {b.num()*a.den()},
+	    num {a.num()*b.den()};
+
+	Rational mul {num, den};
+	mul.simplify();
+
+	return mul;
+}
+
+
+
+bool operator== (Rational& a, Rational& b)
 { 
     return a.num()/a.den() == b.num()/b.den();
+}
+
+
+
+bool operator!= (Rational& a, Rational& b)
+{
+	return !(a == b);
+}
+
+
+
+std::ostream& operator << (std::ostream& os, Rational r)
+{
+	os << r.num() << '/' << r.den();
+	return os;
 }
 
 
@@ -53,10 +162,20 @@ int main()
 {
 	try
 	{
-        Rational a {1,2},
-                 b {2,4};
+        Rational a {2,2},
+                 b {5,15},
+				 c {4,8},
+				 d {8,16};
         
-        std::cout << std::operator==(a,b);
+		if (a == b) std::cout << "a and b are equal\n";
+
+		std::cout << "a is " << a << " and b is " << b << '\n';
+		std::cout << "a + b is " << a + b << '\n';
+		std::cout << "a - b is " << a - b << '\n';
+		std::cout << "a * b is " << a * b << '\n';
+		std::cout << "a / b is " << a / b << '\n';
+
+		if (c == d) std::cout << c << " is equal to " << d << '\n';
 
 		return 0;
 	}
@@ -65,10 +184,14 @@ int main()
 		std::cerr << e.what() << '\n';
 		return 1;
 	}
+	catch(Rational::Invalid)
+	{
+		std::cerr << "Denominator can't be zero.\n";
+	}
 	catch(...)
 	{
 		std::cerr << "Unknown error.\n";
-		return 2;
+		return 3;
 	}
 
 }
