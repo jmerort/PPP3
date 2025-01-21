7fe88799d1efc3187e549cfc7f9746ee2c6134aa
jmerort <jmerortz@gmail.com>
2025-01-21 11:52:56 +0100

    begin excercise 14 from chapter 8

---
+++ CH08/8_14.cpp
@@ -0,0 +1,25 @@
+/*
+---
+[14] Design and implement a Money class for calculations involving dollars
+and cents where arithmetic has to be accurate to the last cent using the
+4/5 rounding rule (.5 of a cent rounds up; anything less than .5 rounds
+down). Represent a monetary amount as a number of cents in a long int,
+but input and output as dollars and cents, e.g., $123.45. Do not worry
+about amounts that don’t fit into a long int.
+---
+
+Jan 2025
+@jmerort
+*/
+
+class Money
+{
+    public:
+    
+    private:
+};
+
+int main()
+{
+
+}
